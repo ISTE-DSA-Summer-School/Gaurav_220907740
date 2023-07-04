5212f918a7a67e69e0e7284a4b7de1ba94e6ec8f
Gamma300 <gamma300104@gmail.com>
2023-07-04 16:07:10 +0530

    reverse array

---
+++ main5.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+void reverseArray(int arr[], int size) {
+    int start = 0;
+    int end = size - 1;
+
+    while (start < end) {
+        // Swap elements at start and end indices
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+
+        // Move start and end indices towards the center
+        start++;
+        end--;
+    }
+}
+
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    int size;
+
+    cout << "Enter the size of the array: ";
+    cin >> size;
+
+    int arr[size];
+
+    cout << "Enter the elements of the array: "<<endl;
+    for (int i = 0; i < size; i++) {
+        cin >> arr[i];
+    }
+
+    cout << "Original array: ";
+    printArray(arr, size);
+
+    reverseArray(arr, size);
+
+    cout << "Reversed array: ";
+    printArray(arr, size);
+
+    return 0;
+}
